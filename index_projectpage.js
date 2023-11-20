1267e0edaa7d4b88a653d123a28cc1e0e329ef40
Gabriella Romano <142850559+gbellaromano@users.noreply.github.com>
2023-11-20 17:54:36 -0600

    Add files via upload

--- index_projectpage.js
+++ index_projectpage.js
@@ -1,25 +1,9 @@
-$('.carasol').carasol({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
+$(document).ready(function(){
+  $('.carasol').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: true
   });
+});
+
