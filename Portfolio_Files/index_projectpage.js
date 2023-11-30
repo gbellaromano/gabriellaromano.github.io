273a9831117616bfbc14e365f13ccafee7757cbd
Gabriella Romano <142850559+gbellaromano@users.noreply.github.com>
2023-11-30 14:46:44 -0600

    Add files via upload

---
+++ Portfolio_Files/index_projectpage.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $('.carasol').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 430,
+        settings: {
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+        }
+      }
+    ]
+        
+  });
+});
+
